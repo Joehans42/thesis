8d03bdbe5bda625027eb7adad8ea568c640111a3
Johannes Gade Gyldenkaerne <s164397@student.dtu.dk>
2023-02-07 09:59:35 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='General Super Resolution For Oblique Aerial Images',
+    author='Johannes G. Gyldenkærne',
+    license='MIT',
+)
