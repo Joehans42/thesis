91b1ae1061b303dd593d641f2782d0dee0c21d00
Johannes Gade Gyldenkaerne <s164397@student.dtu.dk>
2023-03-14 11:24:55 +0100

    initial srgan model file

---
+++ src/models/srgan.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+class ConvBlock(nn.Module):
+    pass
+
+class ResidualBlock(nn.Module):
+    pass
+
+class UpsampleBlock(nn.Module):
+    pass
+
+class Generator(nn.Module):
+    pass
+
+class Discriminator(nn.Module):
+    pass
+
+    
